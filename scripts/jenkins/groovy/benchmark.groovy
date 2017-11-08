4256e27e4d87920cbc27d943e1273b2d4b435bcd
Michal Raška <michal.raska@gmail.com>
2017-11-08 12:37:56 +0100

    [ITA-102] Build H2O-3 used for benchmarks

--- scripts/jenkins/groovy/benchmark.groovy
+++ scripts/jenkins/groovy/benchmark.groovy
@@ -1,7 +1,12 @@
 def call(buildConfig, stageConfig) {
 
   def BENCHMARK_SUITE_DEFAULT_PATH = 'benchmarking_suite.csv'
+  def H2O_3_BENCHMARK_HOME = 'h2o-3-benchmark'
 
+  def insideDocker = load('h2o-3/scripts/jenkins/groovy/insideDocker.groovy')
+  def buildTarget = load('h2o-3/scripts/jenkins/groovy/buildTarget.groovy')
+  def customEnv = load('h2o-3/scripts/jenkins/groovy/customEnv.groovy')
+  
   stage (stageConfig.stageName) {
     writeFile file: BENCHMARK_SUITE_DEFAULT_PATH, text: """id,commit,num_of_runs,benchmark_file,env_file,bucket
 1,master,1,benchmark.sh,env-file,test.0xdata.com/h2o-3-benchmarks
@@ -31,17 +36,76 @@ echo \${TEST_VAR}
       configPath = BENCHMARK_SUITE_DEFAULT_PATH
     }
     def benchmarkConfig = readConfig(configPath)
-    echo "${benchmarkConfig}"
+
+    benchmarkConfig.each{ id, spec ->
+      def revisionResultsRoot = resultsRoot + "/${spec['commit']}_${id}"
+      echo "###### Starting benchmarks for ${spec['commit']} ######"
+      echo """Details:
+      Revision:           ${spec['commit']}
+      Benchmark script:   ${spec['benchmarkFile']}
+      Environment file:   ${spec['envFile']}
+      Results folder:     ${revisionResultsRoot}
+      # od runs:          ${spec['numOfRuns']}
+      Bucket for results: ${spec['bucket']}
+      """
+
+      dir (H2O_3_BENCHMARK_HOME) {
+        retry(3) {
+          timeout(time: 1, unit: 'MINUTES') {
+            checkout([$class: 'GitSCM', branches: [[name: spec['commit'] ]],
+              userRemoteConfigs: [[url: 'https://github.com/h2oai/h2o-3']]])
+          }
+        }
+      }
+      def buildEnv = customEnv() + ["PYTHON_VERSION=${stageConfig.pythonVersion}", "R_VERSION=${stageConfig.rVersion}"]
+      insideDocker(buildEnv, buildConfig, stageConfig.timeoutValue, 'MINUTES') {
+        buildTarget {
+          target = 'build-h2o-3'
+          hasJUnit = false
+          archiveFiles = false
+          h2o3dir = H2O_3_BENCHMARK_HOME
+        }
+      }
+
+      // def range = (1..Integer.parseInt(spec['numOfRuns'])).toArray()
+      // for (runNum in range) {
+      //   try {
+      //     echo "###### Starting benchmark #${runNum} for ${spec['commit']} ######"
+      //     def timestamp = new Date().getTime()
+      //     if (!ensureFileExists(spec['benchmarkFile']) || !ensureFileExists(spec['envFile'])) {
+      //         continue
+      //     }
+      //
+      //     def benchScriptAbsolutePath = env.WORKSPACE + '/' + spec['benchmarkFile']
+      //     def envFileAbsolutePath = env.WORKSPACE + '/' + spec['envFile']
+      //
+      //     // insideDockerEnv(....) {
+      //       dir(revisionResultsRoot) {
+      //         dir ("run${runNum}_${timestamp}") {
+      //           def envList = []
+      //           envList.addAll(readFile(envFileAbsolutePath).split('\n'))
+      //           withEnv(envList) {
+      //             sh "chmod +x ${benchScriptAbsolutePath}"
+      //             sh "${benchScriptAbsolutePath}"
+      //           }
+      //         }
+      //       }
+      //     // }
+      //   } finally {
+      //     echo "###### Benchmark #${runNum} for ${spec['commit']} finished ######"
+      //   }
+      // }
+    }
   }
 }
 
 def readConfig(final String configPath) {
     if (fileExists(configPath)) {
-        def config = [:]
+        def benchmarkConfig = [:]
         readFile(configPath).split('\n').each{ line, count ->
             def values = line.split(",\\s*")
             if (values[0].toLowerCase() != 'id') {
-                config[values[0]] = [
+                benchmarkConfig[values[0]] = [
                     commit: values[1],
                     numOfRuns: values[2],
                     benchmarkFile: values[3],
@@ -50,7 +114,7 @@ def readConfig(final String configPath) {
                 ]
             }
         }
-        return config
+        return benchmarkConfig
     }
     error 'CSV file not found'
 }
@@ -62,83 +126,5 @@ boolean ensureFileExists(final String path) {
     }
     return true
 }
-
-// def wip() {
-//     stage ('Preparation') {
-//
-//         writeFile file: BENCHMARK_SUITE_DEFAULT_PATH, text: """id,commit,num_of_runs,benchmark_file,env_file,bucket
-// 1,master,1,benchmark.sh,env-file,test.0xdata.com/h2o-3-benchmarks
-// 2,rel-weierstrass,3,benchmark.sh,env-file,test.0xdata.com/h2o-3-benchmarks
-// """
-//         writeFile file: 'env-file', text: """DUMMY_VAR='BENCH VAR'
-// TEST_VAR='TEST VAR'
-// """
-//
-//         writeFile file: 'benchmark.sh', text: """#! /bin/bash
-// echo Benchmark test
-// printenv
-// echo \${DUMMY_VAR}
-// echo \${TEST_VAR}
-// """
-//     }
-//
-//     stage ('Benchmark') {
-//
-//         config.each{ id, spec ->
-//             def revisionResultsRoot = resultsRoot + "/${spec['commit']}_${id}"
-//             echo "###### Starting benchmarks for ${spec['commit']} ######"
-//             echo """Details:
-//             Revision:           ${spec['commit']}
-//             Benchmark script:   ${spec['benchmarkFile']}
-//             Environment file:   ${spec['envFile']}
-//             Results folder:     ${revisionResultsRoot}
-//             # od runs:          ${spec['numOfRuns']}
-//             Bucket for results: ${spec['bucket']}
-//             """
-//
-//             dir ('h2o-3-benchmark') {
-//                 retry(3) {
-//                     timeout(time: 1, unit: 'MINUTES') {
-//                         checkout([$class: 'GitSCM', branches: [[name: spec['commit'] ]],
-//                             userRemoteConfigs: [[url: 'https://github.com/h2oai/h2o-3']]])
-//                     }
-//                 }
-//             }
-//
-//             // insideDocker(....) {
-//             //      buildH2O3()
-//             // }
-//
-//             def range = (1..Integer.parseInt(spec['numOfRuns'])).toArray()
-//             for (runNum in range) {
-//                 try {
-//                     echo "###### Starting benchmark #${runNum} for ${spec['commit']} ######"
-//                     def timestamp = new Date().getTime()
-//                     if (!ensureFileExists(spec['benchmarkFile']) || !ensureFileExists(spec['envFile'])) {
-//                         continue
-//                     }
-//
-//                     def benchScriptAbsolutePath = env.WORKSPACE + '/' + spec['benchmarkFile']
-//                     def envFileAbsolutePath = env.WORKSPACE + '/' + spec['envFile']
-//
-//                     // insideDockerEnv(....) {
-//                         dir(revisionResultsRoot) {
-//                             dir ("run${runNum}_${timestamp}") {
-//                                 def envList = []
-//                                 envList.addAll(readFile(envFileAbsolutePath).split('\n'))
-//                                 withEnv(envList) {
-//                                     sh "chmod +x ${benchScriptAbsolutePath}"
-//                                     sh "${benchScriptAbsolutePath}"
-//                                 }
-//                             }
-//                         }
-//                     // }
-//                 } finally {
-//                     echo "###### Benchmark #${runNum} for ${spec['commit']} finished ######"
-//                 }
-//             }
-//         }
-//     }
-// }
 
 return this
